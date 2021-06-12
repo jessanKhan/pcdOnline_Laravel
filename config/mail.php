4715aab64e96db5d62323ca0d2510cfeb643ce8f
nishat141110 <80410749+nishat141110@users.noreply.github.com>
2021-06-12 20:21:11 +0600

    Update mail.php
    config edit

--- config/mail.php
+++ config/mail.php
@@ -36,8 +36,8 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp-relay.sendinblue.com'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST', 'smtp.dreamhost.com'),
+            'port' => env('MAIL_PORT', 465),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
